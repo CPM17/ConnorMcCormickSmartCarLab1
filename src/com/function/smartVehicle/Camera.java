a785c515254501a5654180a9cef654a72a07ba4f
Connor McCormick <Connor McCormick@DESKTOP-BECHNFG>
2021-11-05 21:44:42 -0400

    created classes and noted a brief description

---
+++ src/com/function/smartVehicle/Camera.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.function.smartVehicle;
+
+/**
+ * @author Connor McCormick
+ *
+ */
+public class Camera {
+
+}
---
+++ src/com/function/smartVehicle/SmartVehicle.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.function.smartVehicle;
+
+/**
+ * @author Connor McCormick
+ *I created my primary class as the smart vehicle just like the bicycle I made its components subclasses... hopefuly
+ */
+public class SmartVehicle {
+	private string serialNum;
+	
+	public bool systemCheck ();
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
