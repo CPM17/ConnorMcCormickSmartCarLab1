0eb06e5f6d613d6057fc393599f026266da00f5c
Connor McCormick <Connor McCormick@DESKTOP-BECHNFG>
2021-11-20 09:26:52 -0500

    update

--- src/com/function/smartVehicle/SmartCarComputer.java
+++ src/com/function/smartVehicle/SmartCarComputer.java
@@ -7,7 +7,20 @@ package com.function.smartVehicle;
  * @author Connor McCormick
  *I created my primary class as the smart vehicle just like the bicycle I made its components subclasses... hopefuly
  */
-public class SmartVehicle {
+public class SmartCarComputer {
+	private RasPi computer;
+	private drive LeftDrive;
+	private drive RightDrive;
+	private UltraTrans DistSens;
+	
+	
+	public SmartCarComputer() {
+	computer = new RasPi ();
+	}
+	
+	public void SystemCheck () {
+		// TODO - 
+	}
 
 
 	/**
